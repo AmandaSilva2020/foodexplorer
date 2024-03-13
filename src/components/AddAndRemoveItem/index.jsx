99323b95f4e92544395aff382c6bb134a73fbaa2
Amanda Silva <amanda328@hotmail.com>
2024-03-13 21:07:06 +0000

    add and remove function

--- src/components/AddAndRemoveItem/index.jsx
+++ src/components/AddAndRemoveItem/index.jsx
@@ -1,15 +1,27 @@
+import { useState } from "react";
+
 import { Container, AddAndRemove } from "./styles";
-import { Button } from "../Button"
+import { Button } from "../Button";
 
 import { PiPlus, PiMinus } from "react-icons/pi";
 
 export function AddAndRemoveItem({btnExtists, ...rest }){
+    const[orderQnty, setOrderQnty] = useState(0);
+    
+    function addToOrder(){
+        setOrderQnty(orderQnty + 1);
+    }
+
+    function removeFromOrder(){
+        setOrderQnty(orderQnty - 1);
+    }
+
     return(
         <Container>
             <AddAndRemove>
-                <button><PiMinus /></button>
-                <span>01</span>
-                <button><PiPlus /></button>
+                <button onClick={removeFromOrder}><PiMinus /></button>
+                <span className="orderQnty">{orderQnty}</span>
+                <button onClick={addToOrder}><PiPlus /></button>
             </AddAndRemove>
 
             {btnExtists ? <Button title="Incluir" /> : ""}
